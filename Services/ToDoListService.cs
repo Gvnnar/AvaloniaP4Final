947d790e802102799d2d9b251ddb93cdc0f7f144
Gvnnar <132749163+Gvnnar@users.noreply.github.com>
2024-08-01 19:13:10 +0200

    Add files via upload

---
+++ Services/ToDoListService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using ToDoList.DataModel;
+
+namespace ToDoList.Services
+{
+    public class ToDoListService
+    {
+        public IEnumerable<ToDoItem> GetItems()
+        {
+            // Returning an empty list since items are now loaded from file
+            return new List<ToDoItem>();
+        }
+    }
+}
